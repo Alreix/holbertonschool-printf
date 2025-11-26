3e74039925d66db4fcf27f4aca2b5ba794ab3ce3
Alreix <alreixdev@gmail.com>
2025-11-26 14:07:03 +0100

    fix(_printf): avoid infinite loop when '%' appears at end of format string

--- _printf.c
+++ _printf.c
@@ -16,23 +16,23 @@ int _printf(const char *format, ...)
 {
 	int i = 0, j = 0, len = 0;
 	va_list args;
-	printer_t printer[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+	printer_t printer[] = {{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'d', print_int}, {'i', print_int}, {'\0', NULL}};
-	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	while (format && format[i] != '\0')
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
 		{
 			len += _putchar(format[i]);
 			i++;
 		}
-		else if (format[i] == '%' && format[i + 1] != '\0')
+		else if (format[i] == '%')
 		{
-			j = 0;
-			while (printer[j].index != '\0')
+			if (format[i + 1] == '\0')
+				return(-1);
+			for (j = 0; printer[j].index != '\0'; j++)
 			{
 				if (format[i + 1] == printer[j].index)
 				{
@@ -40,7 +40,6 @@ int _printf(const char *format, ...)
 					i += 2;
 					break;
 				}
-				j++;
 			}
 			if (printer[j].index == '\0')
 			{
